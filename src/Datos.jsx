ef46a14c45c314ef53d9336f8321e313426c3a53
Alejandro Lugo Fumero <alu0101329185@ull.edu.es>
2022-10-19 17:44:22 +0100

    Métodos de predicción

--- src/Datos.jsx
+++ src/Datos.jsx
@@ -87,10 +87,14 @@ export function Datos() {
             setErrorArchivo(
               `Error, hay que seleccionar un archivo que contenga la matriz`
             );
-          if (!numeroVecinos)
-            setErrorVecinos(`Error, hay que seleccionar un numero de vecinos`);
+          if (!numeroVecinos || numeroVecinos < 1)
+            setErrorVecinos(
+              `Error, el número de vecinos tiene que ser un valor mayor que 0`
+            );
           if (archivoData && numeroVecinos) {
-            generarVecinos(formateo(archivoData, rangoMax, rangoMin), metrica);
+            let formato = formateoMatriz(archivoData, rangoMax, rangoMin);
+            let genVec = generarVecinos(formato, metrica);
+            resolverIncognitas(formato, genVec, numeroVecinos, prediccion);
           }
         }}
       >
@@ -100,22 +104,30 @@ export function Datos() {
   );
 }
 
+
+
 /*Arreglamos la matriz de entrada*/
-function formateo(archivoData, rangoMax, rangoMin) {
-  let arraysTextoArchivo = archivoData.split("\n");
+function formateoMatriz(archivoData, rangoMax, rangoMin) {
+  // Separamos la matriz por \n
+  let arraysTextoArchivoEspacios = archivoData.split("\n");
+  let arraysTextoArchivo = [];
+  // Quetamos los espacios
+  for (let i = 0; i < arraysTextoArchivoEspacios.length - 1; i++) {
+    arraysTextoArchivo.push(arraysTextoArchivoEspacios[i].split(" "));
+  }
 
   let matriz = [];
-  for (let i = 0; i < arraysTextoArchivo.length - 1; i++) {
+  // pasamos los numeros que se encuentran en string a float y normalizamos los datos 
+  // si nos encontramos un - lo sustituimos por -1
+  for (let i = 0; i < arraysTextoArchivo.length; i++) {
     let aux = [];
     for (let j = 0; j < arraysTextoArchivo[i].length - 1; j++) {
-      if (arraysTextoArchivo[i][j] !== " ") {
-        if (arraysTextoArchivo[i][j] === "-") aux.push(-1);
-        else
-          aux.push(
-            (parseInt(arraysTextoArchivo[i][j], 10) - rangoMin) /
-              (rangoMax - rangoMin)
-          );
-      }
+      if (arraysTextoArchivo[i][j] === "-") aux.push(-1);
+      else
+        aux.push(
+          (parseInt(arraysTextoArchivo[i][j], 10) - rangoMin) /
+            (rangoMax - rangoMin)
+        );
     }
     matriz.push(aux);
   }
@@ -123,12 +135,105 @@ function formateo(archivoData, rangoMax, rangoMin) {
   return matriz;
 }
 
+
+
+// Calculamso el indice de los mejores vecinos
+function calcularIndexMejoresVecinos(matriz, posicion, arrayVecinos, numeroVecinos) {
+  // ordenamos los vecinos de mayor a menor.
+  let mayoresValores = arrayVecinos
+    .slice()
+    .sort(function (a, b) {
+      return b - a;
+    })
+    .splice(0, arrayVecinos.length);
+
+  let indexMejVal = [];
+  // hacemos una copia de los valores de los vecinos.
+  let copiaArrayVecinos = arrayVecinos.slice(0);
+
+  // Sacamos tantos vecinos como el usuario haya indicado sin repetirlos
+  for (let i = 0; i < mayoresValores.length - 1; i++) {
+    for (let j = 0; j < copiaArrayVecinos.length; j++) {
+      if (
+        mayoresValores[i] === copiaArrayVecinos[j] &&
+        matriz[j][posicion] !== -1 &&
+        indexMejVal.length < numeroVecinos
+      ) {
+        indexMejVal.push(j);
+        copiaArrayVecinos[j] = -1;
+        break;
+      }
+    }
+  }
+
+  return indexMejVal;
+}
+
+
+
+// Realizamos la predicción.
+function prediccionSimpleMedia(matriz, posicion, arrayVecinos, numeroVecinos, prediccion) {
+  // calculamos los mejores vecinos.
+  let indexMejVec = calcularIndexMejoresVecinos(matriz, posicion, arrayVecinos, numeroVecinos);
+  let numerador = 0,
+    denominador = 0;
+
+  // Recorremos los vecinos y realizamos el calculo.
+  for (let i = 0; i < indexMejVec.length; i++) {
+    if (prediccion === "Predicción simple.")
+      numerador +=
+        arrayVecinos[indexMejVec[i]] * matriz[indexMejVec[i]][posicion];
+    else
+      numerador +=
+        arrayVecinos[indexMejVec[i]] *
+        (matriz[indexMejVec[i]][posicion] -
+          calcularMedia(matriz[indexMejVec[i]]));
+    denominador += Math.abs(arrayVecinos[indexMejVec[i]]);
+  }
+
+  if ("Predicción simple.") return numerador / denominador;
+  return calcularMedia(matriz[arrayVecinos.indexOf(-1)]) + numerador / denominador
+}
+
+
+
+// Resolvemos las incognitas que se encuentran en la matriz.
+function resolverIncognitas(matriz, vecinos, numeroVecinos, prediccion) {
+  let incognitasResueltas = [];
+
+  // Recorremos la matriz y si encontramos un -1 aplicamos el metodo de predicción.
+  for (let i = 0; i < matriz.length; i++) {
+    let aux = [];
+    for (let j = 0; j < matriz[i].length; j++) {
+      if (matriz[i][j] === -1)
+        aux.push(
+          prediccionSimpleMedia(
+            matriz,
+            j,
+            vecinos[i],
+            numeroVecinos,
+            prediccion
+          )
+        );
+    }
+    incognitasResueltas.push(aux);
+  }
+
+    console.log(incognitasResueltas)
+  return incognitasResueltas;
+}
+
+
+
+// Calculamos los vecinos para cada usuario
 function generarVecinos(matriz, metrica) {
   let vecinos = [];
 
-  for (let i = 0; i < matriz.length - 1; i++) {
+  // Recorremos la matriz por fila y cuando nos encontrmos a algun vecino llamamos
+  // a la metrica escogida. 
+  for (let i = 0; i < matriz.length; i++) {
     let aux = [];
-    for (let j = 0; j < matriz.length - 1; j++) {
+    for (let j = 0; j < matriz.length; j++) {
       if (i !== j) {
         if (metrica === "Correlación de Pearson.")
           aux.push(
@@ -142,13 +247,19 @@ function generarVecinos(matriz, metrica) {
         else if (metrica === "Distancia coseno.")
           aux.push(distanciaCoseno(matriz[i], matriz[j]));
         else aux.push(distanciaEuclidiana(matriz[i], matriz[j]));
+      } else {
+        aux.push(-1);
       }
     }
     vecinos.push(aux);
   }
-  console.log(vecinos);
+
+  return vecinos;
 }
 
+
+
+// Metrica euclidiana
 function distanciaEuclidiana(usuario1, usuario2) {
   let contador = 0,
     distancia = 0;
@@ -164,12 +275,15 @@ function distanciaEuclidiana(usuario1, usuario2) {
   return distancia / Math.sqrt(contador);
 }
 
+
+
+// Metrica distancia coseno
 function distanciaCoseno(usuario1, usuario2) {
   let numerador = 0,
     denominador1 = 0,
     denominador2 = 0;
 
-  for (let i = 0; i < usuario1.length - 1; i++) {
+  for (let i = 0; i < usuario1.length; i++) {
     if (usuario1[i] !== -1 && usuario2[i] !== -1) {
       numerador += usuario1[i] * usuario2[i];
       denominador1 += Math.pow(usuario1[i], 2);
@@ -180,13 +294,16 @@ function distanciaCoseno(usuario1, usuario2) {
   return numerador / (Math.sqrt(denominador1) * Math.sqrt(denominador2));
 }
 
+
+
+// Metrica correlación de Pearson.
 function correlacionPearson(usuario1, media1, usuario2, media2) {
   let numerador = 0,
     denominador1 = 0,
     denominador2 = 0,
     correlacion = 0;
 
-  for (let i = 0; i < usuario1.length - 1; i++) {
+  for (let i = 0; i < usuario1.length; i++) {
     if (usuario1[i] !== -1 && usuario2[i] !== -1) {
       numerador += (usuario1[i] - media1) * (usuario2[i] - media2);
       denominador1 += Math.pow(usuario1[i] - media1, 2);
@@ -198,11 +315,14 @@ function correlacionPearson(usuario1, media1, usuario2, media2) {
   return (correlacion + 1) / 2;
 }
 
+
+
+// Calculamos la media sin tener en cuenta los valores nulos.
 function calcularMedia(vector) {
   let sumatorio = 0,
     contador = 0;
 
-  for (let i = 0; i < vector.length - 1; i++) {
+  for (let i = 0; i < vector.length; i++) {
     if (vector[i] !== -1) {
       sumatorio += vector[i];
       contador++;
