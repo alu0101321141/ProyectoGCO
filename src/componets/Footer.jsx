121d039676fddf4862d586d4cf84506a866c5d76
Vlatko Marchán <alu0101321141@ull.edu.es>
2022-10-22 11:43:14 +0100

    Añadimos los nombres de los integrantes (footer)

---
+++ src/componets/Footer.jsx
@@ -0,0 +1,9 @@
+export function Footer() {
+    return (
+        <footer className=" grid grid-cols-3 place-content-center text-sm sm:mt-0 font-mono font-bold p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-white-">            
+            <ul>Alejandro Lugo Fumero</ul>
+            <ul>Vlatko Marchán Sekulic</ul>
+            <ul>Nicolas Vegas Rodríguez</ul>
+        </footer>
+    );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client'
 import {Datos} from './componets/Datos'
 import { Resultado } from './componets/Resultado'
 import {DataContextProvider} from "./context/DataContext"
+import {Footer} from "./componets/Footer"
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <>
+  <div className="gap-4">
     <DataContextProvider>
       <Datos/>
       <Resultado/>
     </DataContextProvider>
-  </>
+    <Footer/>
+  </div>
 )
